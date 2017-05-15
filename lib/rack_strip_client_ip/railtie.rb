edbaad125f408e1cd103948ecdd730cfbe83652d
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-05-15 11:56:38 +0100

    Pass class rather than string to insert middleware
    This commit changes the gem to pass a class rather than a string when inserting the middleware. This removes a deprecation warning for Rails 5.

--- lib/rack_strip_client_ip/railtie.rb
+++ lib/rack_strip_client_ip/railtie.rb
@@ -1,7 +1,11 @@
 module RackStripClientIp
   class Railtie < Rails::Railtie
     initializer "rack_strip_client_ip.insert_middleware" do |app|
-      app.config.middleware.insert_before 0, "RackStripClientIp::Middleware"
+      if Rails::VERSION::MAJOR >= 5
+        app.config.middleware.insert_before 0, RackStripClientIp::Middleware
+      else
+        app.config.middleware.insert_before 0, "RackStripClientIp::Middleware"
+      end
     end
   end
 end
