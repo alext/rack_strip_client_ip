e14bb5a27dc320a997fe84c2e85a4c707e84e39a
Ikenna N. Okpala <me@ikennaokpala.com>
2017-05-19 12:49:22 +0100

    Fix rails 5 deprecation warning
    This commit replaces the string reference to RackStripClientIp::Middleware,
    because passing strings or symbols to middleware builder is now deprecated
    in rails 5,

--- lib/rack_strip_client_ip/railtie.rb
+++ lib/rack_strip_client_ip/railtie.rb
@@ -1,7 +1,7 @@
 module RackStripClientIp
   class Railtie < Rails::Railtie
     initializer "rack_strip_client_ip.insert_middleware" do |app|
-      app.config.middleware.insert_before 0, "RackStripClientIp::Middleware"
+      app.config.middleware.insert_before 0, RackStripClientIp::Middleware
     end
   end
 end
